811df2740827dbb8a2d3abded13a5379c07a20a9
pieterdebot <pieter.de.bot@gmail.com>
2021-06-23 16:17:22 +0200

    Made minecraft 1.8 biome mapping compatible with java 16.

--- 1_8_R3/src/main/java/com/pieterdebot/biomemapping/version/Wrapper_1_8_R3.java
+++ 1_8_R3/src/main/java/com/pieterdebot/biomemapping/version/Wrapper_1_8_R3.java
@@ -1,7 +1,6 @@
 package com.pieterdebot.biomemapping.version;
 
 import com.pieterdebot.biomemapping.Biome;
-import com.pieterdebot.biomemapping.utils.NMSUtils;
 import net.minecraft.server.v1_8_R3.BiomeBase;
 
 import java.lang.reflect.Field;
@@ -19,21 +18,12 @@ public class Wrapper_1_8_R3 implements VersionWrapper {
     }
 
     @Override
-    public void replaceBiomes(Biome oldBiome, Biome newBiome) throws Exception{
-        Field biomesField = BiomeBase.class.getDeclaredField("biomes");
-        biomesField.setAccessible(true);
-        NMSUtils.removeFinal(biomesField);
+    public void replaceBiomes(Biome oldBiome, Biome newBiome) throws Exception {
+        BiomeBase[] biomes = BiomeBase.getBiomes();
 
-        Field idField = BiomeBase.class.getDeclaredField("id");
-        idField.setAccessible(true);
-
-        BiomeBase[] biomes = (BiomeBase[]) biomesField.get(null);
-
-        BiomeBase oldBiomeBase = getBiomeBase(oldBiome);
         BiomeBase newBiomeBase = getBiomeBase(newBiome);
 
-        biomes[oldBiomeBase.id] = newBiomeBase;
-        biomesField.set(null, biomes);
+        biomes[oldBiome.getId()] = newBiomeBase;
     }
 
     private BiomeBase getBiomeBase(Biome biome) throws ReflectiveOperationException{
